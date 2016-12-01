00a5c68a329bf1a2c921551ae8c5253af279360b
Kevin Turner <turner.1304@osu.edu>
2016-12-01 11:56:04 -0500

    Changed to use floating point precision

--- PDFs/ExpGausPdf.cu
+++ PDFs/ExpGausPdf.cu
@@ -1,14 +1,14 @@
 #include "ExpGausPdf.hh"
 
 EXEC_TARGET fptype device_ExpGaus (fptype* evt, fptype* p, unsigned int* indices) {
-  fptype x     = evt[indices[2 + indices[0]]]; 
-  fptype mean  = p[indices[1]];
-  fptype sigma = p[indices[2]];
-  fptype alpha = p[indices[3]];
+  float x     = evt[indices[2 + indices[0]]]; 
+  float mean  = p[indices[1]];
+  float sigma = p[indices[2]];
+  float alpha = p[indices[3]];
 
-  fptype ret = 0.5*alpha; 
-  fptype exparg = ret * (2*mean + alpha*sigma*sigma - 2*x);
-  fptype erfarg = (mean + alpha*sigma*sigma - x) / (sigma * 1.4142135623);
+  float ret = 0.5*alpha; 
+  float exparg = ret * (2*mean + alpha*sigma*sigma - 2*x);
+  float erfarg = (mean + alpha*sigma*sigma - x) / (sigma * 1.4142135623);
 
   ret *= EXP(exparg); 
   ret *= ERFC(erfarg); 
